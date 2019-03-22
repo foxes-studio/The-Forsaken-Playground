0edbf38a9709120ba06a5a0c66e71b01ba63769e
DhiaKhalfallah <47749870+DhiaKhalfallah@users.noreply.github.com>
2019-03-22 21:55:29 +0100

    Add files via upload

---
+++ Dhia Khalfallah/Separated Headers/Collisions.h
@@ -0,0 +1,11 @@
+#ifndef TACHESD_H_INCLUDED
+#define TACHESD_H_INCLUDED
+
+ bool CollisionBB( SDL_Surface *Player , SDL_Surface *C_Object , SDL_Rect position , SDL_Rect position1 ) ; 
+ bool CollisionTrigoCir ( SDL_Surface *Player , SDL_Surface *C_Object , SDL_Rect Pposition , SDL_Rect Oposition ) ; 
+  bool CollisionTrigoInsc ( SDL_Surface *Player , SDL_Surface *C_Object , SDL_Rect position , SDL_Rect position1  ) ; 
+  
+
+
+
+#endif // TACHESD_H_INCLUDED
---
+++ Dhia Khalfallah/Separated Headers/Objets.h
@@ -0,0 +1,13 @@
+#ifndef TACHESD_H_INCLUDED
+#define TACHESD_H_INCLUDED
+
+
+
+SDL_Surface* Init_Object( SDL_Surface *Object ,SDL_Rect *image ,  int x , int y , char Name[]) ; 
+ void Show_Object (SDL_Surface *Object , SDL_Surface *screen , SDL_Rect Rect  ) ; 
+
+  
+
+
+
+#endif // TACHESD_H_INCLUDED
