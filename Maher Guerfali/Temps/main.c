7d1ed30c5fb648883659c9a37bb37a128acbc3a0
Maher Guerfali <47715285+Maher-Guerfali@users.noreply.github.com>
2019-05-06 17:34:28 +0100

    Create main.c

---
+++ Maher Guerfali/Temps/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include <SDL/SDL_ttf.h>
+#include"SDL/SDL.h"
+#include"SDL/SDL_image.h"
+#include"SDL/SDL_mixer.h"
+
+int main()
+{
+  
+
+
+
+
+monguela();
+
+
+}
